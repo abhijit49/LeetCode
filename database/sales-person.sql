ac54bd622b997810c63208b3f182cbc268680cbb
abhijit <91731000+abhijit49@users.noreply.github.com>
2024-03-11 21:57:38 +0530

    :ballot_box_with_check: sales-person [Runtime: 2695 ms; Memory: 0B]

---
+++ database/sales-person.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select name from SalesPerson 
+where sales_id not in(Select distinct sales_id from Orders
+where com_id = (select com_id from Company where name='RED'));
